8bcc916d6c06de96703fbdb44dc8455dacb063f1
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-03-31 15:27:09 -0400

    my_transpose

--- lib/classwork.rb
+++ lib/classwork.rb
@@ -13,3 +13,20 @@ def my_uniq(array)
     hash.keys
 end
 
+class Array 
+    def two_sum
+        newArr = []
+        self.each_with_index do |ele1,idx1|
+            self.each_with_index do |ele2, idx2|
+                if ele1 + ele2 == 0 && idx2 > idx1
+                    newArr << [idx1,idx2]
+                end
+            end
+        end
+        newArr
+    end
+end
+
+def my_transpose(array)
+    array.transpose
+end
--- spec/classwork_spec.rb
+++ spec/classwork_spec.rb
@@ -33,3 +33,34 @@ describe "#my_uniq" do
         expect(my_uniq([])).to be_empty
     end
 end
+
+describe "Array#two_sum" do
+    let(:array) {[-1, 0, 2, -2, 1]}
+    it "returns the answer" do 
+        expect(array.two_sum).to eq([[0,4], [2,3]])
+    end
+
+    it "returns nil for empty array" do 
+        expect([].two_sum).to be_empty
+    end
+end
+
+describe "#my_transpose" do   
+    let(:array) {[
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+      ] }
+
+    before(:each) do
+        expect(array).not_to receive(:transpose)
+    end
+
+    it "returns the answer" do 
+        expect(my_transpose(array)).to eq([
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+          ])
+    end
+end
