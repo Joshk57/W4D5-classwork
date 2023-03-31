ac7f4dae6fe449d36f7aa97e0a4ad303164897da
joshua <joshk.5793@gmail.com>
2023-03-31 13:43:30 -0400

    my_uniq

--- lib/classwork.rb
+++ lib/classwork.rb
@@ -1,5 +1,15 @@
-class Array
-    def uniq(array)
-        # array.uniq
-    end
+
+def uniq?(array)
+    # return [] if array.empty?
+    array.uniq
 end
+
+def my_uniq(array)
+    hash = Hash.new(0)
+
+    array.each do |num|
+        hash[num] += 1
+    end
+    hash.keys
+end
+
--- spec/classwork_spec.rb
+++ spec/classwork_spec.rb
@@ -1,15 +1,35 @@
 require 'classwork'
 require 'rspec'
 
-describe "Array#uniq" do 
-    let(:array) { [1, 2, 1, 3, 3] }
-    it "requires array" do 
-        expects(array.uniq).to be([1,2,3])
+describe "#uniq?" do 
+    let(:array1) { [1, 2, 1, 3, 3] }
+    let(:array2) {[]}
+
+    it "returns uniq numbers" do 
+        expect(uniq?(array1)).to eq([1,2,3])
     end
 
     it "returns nil for empty array" do 
-        expects([].uniq).to be_nil
+        expect(uniq?([])).to be_empty
     end
 
 end
 
+describe "#my_uniq" do
+    let(:array3) { [1, 2, 1, 3, 3] }
+    let(:array4) {[]}
+
+    before(:each) do
+        expect(array3).not_to receive(:uniq)
+    end
+
+
+
+    it "returns uniq numbers" do 
+        expect(my_uniq(array3)).to eq([1,2,3])
+    end
+
+    it "returns nil for empty array" do 
+        expect(my_uniq([])).to be_empty
+    end
+end
